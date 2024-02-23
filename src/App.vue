88fd3d3f7ef33f5b908cc62798c185afb2c8b38c
BeastPT <ajfa2004@gmail.com>
2024-02-23 17:26:02 +0000

    setup vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Register from './views/Register.vue';
+</script>
+
+<template>
+    <!-- <nav>
+      <RouterLink to="/register">Register</RouterLink>
+    </nav>
+    
+    <RouterView /> -->
+    <Register />
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div class="bg-gradient-to-b from-sky-300 to-slate-400 h-screen w-screen flex justify-center items-center">
+        <div class="flex columns-2 w-[72rem] h-[46rem] rounded-xl overflow-hidden shadow relative">
+            <!--  Register  -->
+            <div class="w-4/6 mx-auto bg-white">
+            </div>
+
+            <!--  Middle Icon  -->
+            <div class="flex z-1 absolute top-1/2 left-[48rem] transform -translate-x-1/2 -translate-y-1/2">
+                <button imag>
+                    <img class="rounded-full" src="https://via.placeholder.com/80" alt="SkillsWapp Logo" />
+                </button>
+            </div>
+
+            <!--  Login  -->
+            <div class="w-2/6 bg-gray-900 text-white">
+                <div>
+                    SKILLSWAPP Logo
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+</style>

22c27ab1b486f921865840c82f62db8edad812d9
BeastPT <ajfa2004@gmail.com>
2024-02-27 23:00:18 +0000

    register ui

--- src/components/Auth/Fields.vue
+++ src/components/Auth/Fields.vue
@@ -8,8 +8,8 @@
 </script>
 
 <template>
-    <div class="relative w-96 rounded-xl mb-6 overflow-hidden">
-        <input type="text" class="bg-zinc-300 text-gray-900 rounded-xl text-sm block w-full ps-10 p-2.5 placeholder-black placeholder-opacity-50 focus:border-black" :placeholder="placeholder" />
+    <div class="relative w-96 h-14 rounded-xl mb-6 overflow-hidden">
+        <input type="text" class="bg-zinc-300 text-gray-900 rounded-xl text-sm block w-full h-full ps-12 p-2.5 placeholder-black border-none placeholder-opacity-50 focus:border-black" :placeholder="placeholder" />
         
         <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none text-black text-opacity-50">
             <slot name="icon">
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -10,12 +10,12 @@
     <div class="bg-gradient-to-b from-sky-300 to-slate-400 h-screen w-screen flex justify-center items-center">
         <div class="flex columns-2 w-[72rem] h-[46rem] rounded-xl overflow-hidden shadow relative">
             <!--  Register  -->
-            <div class="w-4/6 mx-auto bg-white flex flex-col items-center">
+            <div class="w-7/12 mx-auto bg-white flex flex-col items-center">
                 <h1 class="text-gray-900 text-3xl font-poppins font-bold mt-20">Preencha os seus dados</h1>
                 <div class="my-11">
                     Socials
                 </div>
-                <form action="" class="flex flex-col items-center">
+                <form action="" class="flex flex-col items-center h-full">
                     <Field :placeholder="'Nome de utilizador'">
                         <template #icon>
                             <IPerson/>
@@ -32,31 +32,45 @@
                     </Field>
                     <div class="flex items-start mb-6">
                         <div class="flex items-center h-5">
-                            <input id="terms" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded-full bg-gray-50 focus:ring-2 focus:ring-green-600 text-green-600" required />
+                            <input id="terms" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded-full bg-gray-50 focus:ring-2 focus:ring-pink-600 text-pink-600" required />
                         </div>
                         <label for="terms" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Eu aceito os Termos e Condições.</label>
                     </div>
-                    <input type="submit" value="Criar Conta" class="text-white text-2xl font-semibold font-poppins bg-gray-900 rounded-full w-56 h-16 mt-10">
+                    <input type="submit" value="Criar Conta" class="text-white text-2xl font-semibold font-poppins bg-gray-900 rounded-full w-56 h-16 mt-auto mb-32">
                 </form>
             </div>
 
             <!--  Middle Icon  -->
-            <div class="flex z-1 absolute top-1/2 left-[48rem] transform -translate-x-1/2 -translate-y-1/2">
-                <button imag>
+            <div class="flex z-1 absolute top-1/2 left-[42rem] transform -translate-x-1/2 -translate-y-1/2">
+                <button>
                     <img class="rounded-full" src="https://via.placeholder.com/80" alt="SkillsWapp Logo" />
                 </button>
             </div>
 
             <!--  Login  -->
-            <div class="w-2/6 bg-gray-900 text-white">
-                <div>
-                    SKILLSWAPP Logo
+            <div class="w-5/12 bg-gray-900 text-white flex flex-col items-center">
+                <div class="ml-auto mt-11 mr-14">
+                    <span>SKILLSWAPP Logo</span>
+                </div>
+
+                <div class="ml-auto mt-44 mr-14 border-r-8 border-pink-600 pr-3 w-32">
+                    <h1 class="text-2xl font-bold text-end">Crie já a sua conta</h1>
+                </div>
+
+                <div class="text-center mx-14 mt-14 text-base font-normal">
+                    <p>Preencha o seu nome de utilizador, e-mail e senha para explorar todas as funcionalidades disponiveis</p>
+                </div>
+
+                <div class="relative flex flex-col items-center mt-auto mb-32">
+                    <button class="text-white text-2xl font-semibold font-poppins bg-gray-900 rounded-full w-48 h-16 border-white border-2 mt-auto">
+                        Entrar
+                    </button>
+                    <span class="text-sm absolute top-16 pt-2">Já tem conta? Faça o login</span>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
 
 <style scoped>
 </style>

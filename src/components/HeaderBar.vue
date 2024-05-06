e5f6908224d49f2dde3b5de51b8afdacbe45925e
Smaash7 <128426063+Smaash7@users.noreply.github.com>
2024-05-06 23:45:56 +0100

    Update
    Header quase feito, falta o icone e fazer para quando ja esta logado ou nao

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,44 +1,62 @@
 <template>
-    <div class="flex flex-col items-center justify-center">
-      <div class="bg-gray-900 w-full h-40 flex flex-row text-white items-center justify-between">
-        <div class="ml-4 md:ml-8 lg:ml-16">
-          <span>SkillsWapp Logo</span>
+    <div class="bg-gray-900">
+      <nav class="container px-6 py-20 mx-auto md:flex md:justify-between md:items-center">
+        <div class="flex items-center justify-between w-full"> <!-- Ajuste de largura -->
+          <button class="text-xl font-bold text-gray-100 md:text-2xl hover:text-indigo-400">
+            <img src="" alt="Logo" class="h-6 w-6">
+          </button>
+          
+
+          <div class="hidden md:flex items-center justify-end w-full md:w-auto">  
+            <ul class="flex space-x-16">
+              <li class="text-xl font-bold text-gray-100 hover:text-indigo-400">Home</li>
+              <li class="text-xl font-bold text-gray-100 hover:text-indigo-400">About Us</li>
+              <li class="text-xl font-bold text-gray-100 hover:text-indigo-400">Chat</li>
+              <li class="text-xl font-bold text-gray-100 hover:text-indigo-400">Comunidade</li>
+            </ul>
+  
+        
+            <button type="button" class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400 ml-auto">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
+              </svg>
+              
+            </button>
+          </div>
+          
+            <div class="flex md:hidden justify-between"> <!-- Adicionado o estilo justify-between -->
+              <button @click="toggleNav" type="button" class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400 mr-4">
+                <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                  <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                </svg>
+              </button>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-4" fill="none" viewBox="0 0 24 24" stroke="white">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
+              </svg>
+            </div>
+  
+  
         </div>
   
-        <nav class="flex md:justify-center items-center">
-          <ul class="flex flex-row gap-5 text-xl">
-            <li class="hidden sm:block">Home</li>
-            <li class="hidden sm:block">Services</li>
-            <li class="hidden sm:block">Projects</li>
-          </ul>
-        </nav>
-  
-        <div class="mr-4 md:mr-8 lg:mr-16 flex flex-row gap-4">
-          <button>
-            <img class="rounded-full" src="https://via.placeholder.com/32" alt="Search Bar" />
-          </button>
-          <button>
-            <img class="rounded-full" src="https://via.placeholder.com/32" alt="SkillsWapp Logo" />
-          </button>
-        </div>
-      </div>
+        <ul :class="showMenu ? 'flex flex-col items-center' : 'hidden md:flex md:flex-row md:items-center md:space-x-10 md:mt-0'" class="mt-8 space-y-4">
+          <li class="text-xl font-bold text-gray-100 hover:text-indigo-400 md:hidden">Home</li>
+          <li class="text-xl font-bold text-gray-100 hover:text-indigo-400 md:hidden">About Us</li>
+          <li class="text-xl font-bold text-gray-100 hover:text-indigo-400 md:hidden">Chat</li>
+          <li class="text-xl font-bold text-gray-100 hover:text-indigo-400 md:hidden">Comunidade</li>
+        </ul>
+      </nav>
     </div>
   </template>
   
-  <style scoped>
-  @media screen and (max-width: 768px) {
-    .ml-4.md\:ml-8.lg\:ml-16,
-    .mr-4.md\:mr-8.lg\:mr-16 {
-      display: none;
-    }
+  <script>
+  import { ref } from 'vue';
   
-    .hidden.sm\:block {
-      display: block;
-    }
-  
-    .flex.md\:justify-center {
-      justify-content: center;
-    }
-  }
-  </style>
+  export default {
+    setup() {
+      let showMenu = ref(false);
+      const toggleNav = () => (showMenu.value = !showMenu.value);
+      return { showMenu, toggleNav };
+    },
+  };
+  </script>
   

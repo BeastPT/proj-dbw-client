6997d949d39cf693a1e07e7ca928d11e79f31484
Smaash7 <128426063+Smaash7@users.noreply.github.com>
2024-05-09 00:40:46 +0100

    Components Purchase

--- src/components/Retangulos.vue
+++ src/components/Retangulos.vue
@@ -1,35 +1,33 @@
 <template>
-    <div class="flex justify-between mx-4 md:mx-16"> 
-      <div class="w-full sm:w-1/2 lg:w-1/3 mb-8 bg-gray-300 p-2 md:p-3 lg:p-4 rounded-lg"> 
-        <h2 class="text-lg font-semibold mb-3">{{ leftTitle }}</h2> 
-        <div class="flex flex-wrap mb-1 md:mb-2 lg:mb-3"> 
-          <p class="w-full lg:text-left w-1/2">{{ leftFreelancer }}</p>
-          <p class="w-full lg:text-left ">{{ leftProjectCode }}</p>
-          <p class="w-full lg:text-left w-1/3">{{ leftValue }}</p> 
+    <div class="flex justify-between mx-4 md:mx-16">
+      <div class="w-full sm:w-full md:w-5/6 lg:w-2/4 mb-8 bg-gray-300 p-4 rounded-lg">
+        <h2 class="text-lg font-semibold mb-6">{{ Title }}</h2>
+        <div class="flex flex-wrap mb-4">
+          <p class="w-full lg:text-left w-1/2">Freelancer: {{ Name }}</p>
+          <p class="w-full lg:text-left ">Código do projeto: {{ ProjectCode }}</p>
+          <p class="w-full lg:text-left w-1/6">Valor: {{ Value }}</p>
         </div>
         <div class="flex flex-wrap">
-          <p class="w-full lg:text-left ">{{ leftOrderDate }}</p>
+          <p class="w-full lg:text-left ">Data de pedido: {{ OrderDate }}</p>
         </div>
       </div>
   
       <div class="flex items-center">
-        <img :src="imageSrc" :alt="imageAlt" class="w-10 h-10 md:mx-2">
+        <img :src="imageSrc" :alt="imageAlt" class="w-10 h-10 md:max-w-none md:w-auto">
       </div>
   
-      <div class="w-full sm:w-1/2 lg:w-1/3 mb-8 bg-gray-300 p-2 md:p-3 lg:p-4 rounded-lg"> 
-        <h2 class="text-lg font-semibold mb-3">{{ rightTitle }}</h2> 
-        <p>{{ rightStatus }}</p>
+      <div class="w-full sm:w-full md:w-1/2 lg:w-1/3 mb-8 bg-gray-300 p-2 md:p-3 lg:p-4 rounded-lg">
+        <h2 class="text-lg font-semibold mb-3">{{ rightTitle }}</h2>
+        <p>Estado: {{ rightStatus }}</p>
         <p>Entrega final: {{ rightDeliveryDate }}</p>
       </div>
     </div>
   </template>
   
-  
-  
   <script>
   export default {
     props: {
-      leftTitle: String,
+      Title: String,
       Name: String,
       ProjectCode: String,
       Value: String,
--- src/views/Purchase.vue
+++ src/views/Purchase.vue
@@ -10,70 +10,67 @@
       <button class="hover:underline text-gray-900">Favoritos</button>
     </div>
   </div>
-  
+
   <div class="message text-xl font-semibold mb-6 underline mx-8 md:mx-16"> 
     <p class="text-center">As minhas compras</p>
   </div>
-  
-  
-  <div class="flex justify-between mx-4 md:mx-16"> 
-    <div class="w-full sm:w-5/6 lg:w-2/4 mb-8 bg-gray-300 p-4 rounded-lg">
-      <h2 class="text-lg font-semibold mb-6">Tradução de texto de inglês para português</h2>
-      <div class="flex flex-wrap mb-4">
-        <p class="w-full lg:text-left w-1/2">Freelancer: Margarida Ferreira</p>
-        <p class="w-full lg:text-left ">Código do projeto:</p>
-        <p class="w-full lg:text-left w-1/6">Valor: 75€</p>
-      </div>
-      <div class="flex flex-wrap">
-        <p class="w-full lg:text-left ">Data de pedido: 12/02/2024</p>
-      </div>
-    </div>
-  
-    <div class="flex items-center">
-      <img src="@/assets/images/image24.png" alt="Logo1" class="w-10 h-10 md:mx-2">
-    </div>
-  
-   
-    <div class="w-full sm:w-5/6 lg:w-2/4 mb-8 bg-gray-300 p-4 rounded-lg">
-      <h2 class="text-lg font-semibold mb-6">Estado</h2>
-      <p>Concluído</p>
-      <p>Entrega final: 15/02/2024</p>
-    </div>
-  </div>
 
-  <div class="flex justify-between mx-4 md:mx-16"> 
-    <div class="w-full sm:w-5/6 lg:w-2/4 mb-8 bg-gray-300 p-4 rounded-lg">
-      <h2 class="text-lg font-semibold mb-6">Tradução de texto de inglês para português</h2>
-      <div class="flex flex-wrap mb-4">
-        <p class="w-full lg:text-left w-1/2">Freelancer: Margarida Ferreira</p>
-        <p class="w-full lg:text-left ">Código do projeto:</p>
-        <p class="w-full lg:text-left w-1/6">Valor: 75€</p>
-      </div>
-      <div class="flex flex-wrap">
-        <p class="w-full lg:text-left ">Data de pedido: 12/02/2024</p>
-      </div>
-    </div>
-  
-  
-    <div class="flex items-center">
-      <img src="@/assets/images/image24.png" alt="Logo1" class="w-10 h-10 md:mx-2">
-    </div>
-    <div class="w-full sm:w-5/6 lg:w-2/4 mb-8 bg-gray-300 p-4 rounded-lg">
-      <h2 class="text-lg font-semibold mb-6">Estado</h2>
-      <p>Concluído</p>
-      <p>Entrega final: 15/02/2024</p>
-    </div>
-  </div>
+  <div>
+    <Retangulos
+      v-for="(user, index) in users"
+      :key="index"
+      :Title="user.Title"
+      :Name="user.Name"
+      :ProjectCode="user.ProjectCode"
+      :Value="user.Value"
+      :OrderDate="user.OrderDate"
+      :rightTitle="user.rightTitle"
+      :rightStatus="user.rightStatus"
+      :rightDeliveryDate="user.rightDeliveryDate"
+      :imageSrc="user.imageSrc"
+      :imageAlt="user.imageAlt"
+    />
+  </div>      
 </template>
+
+
+<script>
+import Retangulos from '@/components/Retangulos.vue';
+
+export default {
+  components: {
+    Retangulos
+  },
+  data() {
+    return {
+      users: [
+        {
+          Title: 'Tradução de texto de inglês para português',
+          Name: 'Margarida Ferreira',
+          ProjectCode: '123ABC',
+          Value: '75€',
+          OrderDate: '12/02/2024',
+          rightTitle: 'Estado',
+          rightStatus: 'Concluído',
+          rightDeliveryDate: '15/02/2024',
+          imageSrc: 'src/assets/images/image24.png',
+          imageAlt: 'Logo1'
+        },
+      ]
+    };
+  }
+}
+</script>
 
 <style scoped>
 @media (max-width: 1023px) {
-  .flex {
-    flex-direction: column;
-    align-items: center;
-  }
-  .flex > div {
-    margin-bottom: 1rem;
-  }
+.flex {
+  flex-direction: column;
+  align-items: center;
+}
+.flex > div {
+  margin-bottom: 1rem;
+}
 }
 </style>
+

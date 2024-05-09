a2dccdfc881240420b2df69b84214decd2f9f20e
Smaash7 <128426063+Smaash7@users.noreply.github.com>
2024-05-10 00:08:19 +0100

    update

--- src/components/ButtonCountry.vue
+++ src/components/ButtonCountry.vue
@@ -1,9 +1,8 @@
 <template>
-    <button class="relative overflow-hidden transform transition-transform duration-300 hover:scale-105">
-      <img :src="`/images/${image}`" :alt="altText" class="w-full h-auto rounded-lg">
+    <button class="button-container">
+      <img :src="`/images/${image}`" :alt="altText" class="image-container">
       <p class="font-semibold text-left">{{ countryName }}</p>
     </button>
-    
   </template>
   
   <script>
@@ -17,6 +16,32 @@
   </script>
   
   <style scoped>
-
+  .button-container {
+    position: relative; 
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    border-radius: 10px; 
+    overflow: hidden; 
+    transition: transform 0.3s; 
+  }
+  
+  .button-container:hover {
+    transform: scale(1.05); 
+  }
+  
+  .image-container {
+    width: 100%;
+    height: 150px; 
+    object-fit: cover;
+    display: block;
+    border-radius: 10px; 
+  }
+  
+  @media screen and (max-width: 768px) {
+    .image-container {
+      width: 80%; 
+    }
+  }
   </style>
   
